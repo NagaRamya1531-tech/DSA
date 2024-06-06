805ca20a525523115dff4f6aa96a821a333ffbde
Naga Ramya Gurrala <nagaramyagurrala@Nagas-Laptop.lan>
2024-06-06 02:35:23 -0400

    second commit

--- Patterns/pattern2.java
+++ Patterns/pattern2.java
@@ -1,3 +1,15 @@
 public class pattern2 {
-    
+    public static void pattern(int N){
+        for (int i=0;i<N;i++){
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int N=5;
+        pattern(N);
+    }
 }
